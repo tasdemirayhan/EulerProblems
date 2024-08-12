09b134b6a2072f1e779154bf60fad42d408bc232
tasdemirayhan <113140746+tasdemirayhan@users.noreply.github.com>
2024-08-12 16:47:48 +0300

    commit ex9

--- Ex9.java
+++ Ex9.java
@@ -1,25 +1,29 @@
 package Euler;
 
-import java.util.ArrayList;
-
 public class Ex9 {
     public static void main(String[] args) {
-        System.out.println(SpecialPythagoreanTriplet());
+        int[] array = SpecialPythagoreanTriplet(1000);
+
+        if (array != null) {
+            System.out.println(array[0] + "," + array[1] + "," + array[2]);
+            System.out.println(array[0] + array[1] + array[2]);
+        } else {
+            System.out.println("There is no special triplet...");
+        }
     }
-    public static int SpecialPythagoreanTriplet(){
-        int a=0,b=0,c=0;
-        for(int i=1;i<1000;i++){
-            for(int j=i;j<1000;j++){
-                for(int k=i;k<1000;k++){
-                    if(i+j+k==1000 && Math.pow(i, 2)+Math.pow(j, 2)==Math.pow(k, 2)){
-                        return i+j+k;
+
+    public static int[] SpecialPythagoreanTriplet(int sum) {
+        int a = 0, b = 0, c = 0;
+        for (int i = 1; i < sum; i++) {
+            for (int j = i; j < sum; j++) {
+                for (int k = i; k < sum; k++) {
+                    if (i + j + k == sum && Math.pow(i, 2) + Math.pow(j, 2) == Math.pow(k, 2)) {
+                        return new int[] { i, j, k };
                     }
                 }
             }
         }
-        return 0;
-        
-        
-        
+        return null;
+
     }
 }
